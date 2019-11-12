828a5b8f985e7e11dca7b4efb2fc016b158988ff
pcbosca <pcbosca@hotmail.com>
2019-11-12 19:23:29 +0100

    Cambios optimizar ejercicio de imágenes en pestañas

--- js/codigo_tabbed.js
+++ js/codigo_tabbed.js
@@ -7,13 +7,13 @@ function expandImage(img) {
 }
 
 function closeImage(img) {
-    img.parentElement.style.display='none'
+    img.parentElement.style.display = 'none'
 }
 
 var cierra = document.getElementById("cierra")
 cierra.addEventListener("click", function () { closeImage(this) })
 
-var img1 = document.getElementById("img1")
+/* var img1 = document.getElementById("img1")
 img1.addEventListener("click", function () { expandImage(this) })
 
 var img2 = document.getElementById("img2")
@@ -29,4 +29,10 @@ var img5 = document.getElementById("img5")
 img5.addEventListener("click", function () { expandImage(this) })
 
 var img6 = document.getElementById("img6")
-img6.addEventListener("click", function () { expandImage(this) })
+img6.addEventListener("click", function () { expandImage(this) }) */
+
+var imgs = document.querySelectorAll(".row img")
+
+for (var i = 0; i < imgs.length; i++) {
+    imgs[i].addEventListener("click", function () { expandImage(this) })
+}
