a43c5665941607887b54574d9ee74be80b2ab221
pcbosca <pcbosca@hotmail.com>
2019-11-13 09:43:20 +0100

    Cambios optimizar funciones con this

--- js/codigo_tabbed.js
+++ js/codigo_tabbed.js
@@ -1,8 +1,8 @@
-function expandImage(img) {
+function expandImage() {
     var expandImg = document.getElementById("expandedImg");
     var imgText = document.getElementById("imgtext");
-    expandImg.src = img.src;
-    imgText.innerHTML = img.alt;
+    expandImg.src = this.src;
+    imgText.innerHTML = this.alt;
     expandImg.parentElement.style.display = "block";
 }
 
@@ -14,25 +14,25 @@ var cierra = document.getElementById("cierra")
 cierra.addEventListener("click", closeImage)
 
 /* var img1 = document.getElementById("img1")
-img1.addEventListener("click", function () { expandImage(this) })
+img1.addEventListener("click", expandImage)
 
 var img2 = document.getElementById("img2")
-img2.addEventListener("click", function () { expandImage(this) })
+img2.addEventListener("click", expandImage)
 
 var img3 = document.getElementById("img3")
-img3.addEventListener("click", function () { expandImage(this) })
+img3.addEventListener("click", expandImage)
 
 var img4 = document.getElementById("img4")
-img4.addEventListener("click", function () { expandImage(this) })
+img4.addEventListener("click", expandImage)
 
 var img5 = document.getElementById("img5")
-img5.addEventListener("click", function () { expandImage(this) })
+img5.addEventListener("click", expandImage)
 
 var img6 = document.getElementById("img6")
-img6.addEventListener("click", function () { expandImage(this) }) */
+img6.addEventListener("click", expandImage) */
 
 var imgs = document.querySelectorAll(".row img")
 
 for (var i = 0; i < imgs.length; i++) {
-    imgs[i].addEventListener("click", function () { expandImage(this) })
+    imgs[i].addEventListener("click", expandImage)
 }
