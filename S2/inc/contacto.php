5e7bfc183c1d8c1e389b49f896e8468db48700bc
pcbosca <pcbosca@hotmail.com>
2020-01-07 19:41:53 +0100

    Include PHP and navbar responsive

---
+++ S2/inc/contacto.php
@@ -0,0 +1,37 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Portofolio - Contacto</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/estilos.css">
+</head>
+<body>
+<?php
+
+/* incluir el header del web */
+include "header.html";
+?>
+    <div class="topnav" id="myTopnav">
+    <a href="index.php">Inicio</a>
+    <a href="#" class="active">Contacto</a>
+    <a href="javascript:void(0);" class="icon" onclick="desplegar_menu()">
+        <i class="fa fa-bars"></i>
+    </a>
+    </div>
+    <main>
+
+    <h1>Contactar</h1>
+    <img src="images/iconmonstr-facebook-4-120.png" alt="Facebook">
+    <img src="images/iconmonstr-instagram-11-120.png" alt="Instagram">
+    <img src="images/iconmonstr-twitter-1-120.png" alt="Twitter">
+    </main>
+<?php
+
+/* incluir el footer y script del web */
+include "footer.html";
+?>
+</body>
+</html>
---
+++ S2/inc/index.php
@@ -0,0 +1,34 @@
+<!DOCTYPE html>
+<html lang="es">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="ie=edge">
+    <title>Portofolio</title>
+    <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+    <link rel="stylesheet" href="css/estilos.css">
+</head>
+<body>
+<?php
+
+/* incluir el header del web */
+include "header.html";
+?>
+    <div class="topnav" id="myTopnav">
+    <a href="#" class="active">Inicio</a>
+    <a href="contacto.php">Contacto</a>
+    <a href="javascript:void(0);" class="icon" onclick="desplegar_menu()">
+        <i class="fa fa-bars"></i>
+    </a>
+    </div>
+    <main>
+        <h1>Mi porfolio</h1>
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Dolore corrupti impedit deleniti aut qui tempore earum molestiae. Vel, sed, totam repudiandae asperiores officiis mollitia tempore accusamus vero quam molestias unde minima ipsam minus voluptas expedita. Fugiat vero voluptatibus totam non iure deserunt dicta, inventore quisquam. Rem nobis adipisci molestias id nostrum accusamus soluta itaque ea exercitationem? Id similique ratione, alias mollitia atque soluta voluptatem quisquam accusantium consequatur ab iste temporibus omnis. Repudiandae, deserunt facere minima sunt eaque non dicta adipisci perferendis saepe necessitatibus exercitationem mollitia veniam cupiditate itaque quos numquam consequuntur ratione, quidem, corrupti rerum ipsum nesciunt pariatur obcaecati molestiae!
+    </main>
+<?php
+
+/* incluir el footer y script del web */
+include "footer.html";
+?>
+</body>
+</html>
